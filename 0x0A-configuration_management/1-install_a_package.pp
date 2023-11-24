ccb018118245f84bc557740b47f5fab0edec8ec7
Capnot34 <jenniferamachree2@github.com>
2023-11-24 08:28:52 -0800

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -8,4 +8,12 @@ exec { 'install_flask':
   command => '/usr/bin/pip3 install Flask==2.1.0',
   path    => '/usr/bin',
   require => Package['python3-pip'],
+  notify  => Exec['update_bashrc'],
+}
+
+exec { 'update_bashrc':
+  command => 'echo "export PATH=$PATH:/usr/local/bin" >> ~/.bashrc',
+  path    => '/usr/bin',
+  onlyif  => 'test -f ~/.bashrc',
+  require => Exec['install_flask'],
 }
