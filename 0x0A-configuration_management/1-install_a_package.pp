9eaa04ae9d3d5d1aa94aa9cc13a32f828a974704
Capnot34 <jenniferamachree2@github.com>
2023-11-25 01:34:52 -0800

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,12 +3,11 @@
 # Install Flask package
 package { 'flask':
   ensure   => '2.1.0',    # Ensure the Flask package is version 2.1.0
-  provider => '/usr/bin/pip3',  # Use pip3 as the package provider
+  provider => 'pip3',      # Use pip3 as the package provider
 }
 
 # Install Python3 package
 package { 'python3':
   ensure   => 'installed',   # Ensure Python3 is installed
-  provider => '/usr/bin/python3',  # Use python3 as the package provider
+  provider => 'pip3',        # Use pip3 as the package provider
 }
-
