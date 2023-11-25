6ea24946f4783ff2e932987174684ec2546a71e7
Capnot34 <jenniferamachree2@github.com>
2023-11-25 01:54:19 -0800

    not resolved

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,19 +1,6 @@
-# This Puppet manifest installs Flask version 2.1.0 using pip3
+# This Puppet manifest installs puppet-lint
 
-# Install Flask package
-package { 'flask':
-  ensure   => '2.1.0',    # Ensure the Flask package is version 2.1.0
-  provider => 'pip3',      # Use pip3 as the package provider
-}
-
-# Install Python3 package using the system's package manager
-package { 'python3':
-  ensure   => 'installed',   # Ensure Python3 is installed
-  provider => 'apt',         # Use apt as the package provider
-}
-
-# Install puppet-lint gem
 package { 'puppet-lint':
-  ensure   => '2.1.0',    # Ensure puppet-lint is version 2.1.0
-  provider => 'gem',       # Use gem as the package provider
+  ensure   => '2.1.0',
+  provider => 'gem',
 }
