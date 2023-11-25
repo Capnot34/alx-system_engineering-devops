35b6f216bbc8ad54464b09ca16e8f9adcca227f5
Capnot34 <jenniferamachree2@github.com>
2023-11-25 00:43:55 -0800

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,6 @@
-# 1-install_a_package.pp
+# install pupet lint
 
-# Ensure 'python3-pip' is installed
-package { 'python3-pip':
-  ensure => installed,
-}
-
-# Install Flask version 2.1.0 using pip3
-exec { 'install-flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  require => Package['python3-pip'],
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem'
 }
