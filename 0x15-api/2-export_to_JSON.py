3c388a9f68fe40e3f16f59f39b78bc3eaaf1beac
Capnot34 <jenniferamachree2@github.com>
2024-01-02 08:54:06 -0800

    done

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -12,8 +12,10 @@ if __name__ == "__main__":
     session = requests.Session()
 
     employee_id = argv[1]
-    todos_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(employee_id)
-    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)
+    todos_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'
+    .format(employee_id)
+    user_url = 'https://jsonplaceholder.typicode.com/users/{}'
+    .format(employee_id)
 
     employee_response = session.get(todos_url)
     user_response = session.get(user_url)
